ad69ba002cf23674c69a97016adb70e22aec9544
Raghuram Sreenath <raghums@gmail.com>
2020-10-13 20:21:59 +0530

    logging instead of printing.

--- door_switch.py
+++ door_switch.py
@@ -3,15 +3,14 @@ from gpiozero import Button
 from time import sleep
 from Adafruit_IO import Client, RequestError, Feed
 from time import time
+import logging as log
+import os
 
 # Set to your Adafruit IO key.
-# Remember, your key is a secret,
-# so make sure not to publish it when you publish this code!
-ADAFRUIT_IO_KEY = '<key>'
+ADAFRUIT_IO_KEY = os.environ['ADAFRUIT_IO_KEY']
 
 # Set to your Adafruit IO username.
-# (go to https://accounts.adafruit.com to find your username)
-ADAFRUIT_IO_USERNAME = '<username>'
+ADAFRUIT_IO_USERNAME = os.environ['ADAFRUIT_IO_USERNAME']
 
 def refresh_connection():
   # Create an instance of the REST client.
@@ -21,22 +20,27 @@ def refresh_connection():
   try:
       global door
       door = aio.feeds('door')
-  except RequestError: # Doesn't exist, create a new feed
-      # feed = Feed(name="foo")
-      # foo = aio.create_feed(feed)
-      print('No feed called Door.')
+  except RequestError:
+      log.critical('No feed called Door.')
       quit()
 
+log.basicConfig(filename='/var/log/iot/door_switch.log',
+                format='[%(levelname)s]%(asctime)s: %(message)s',
+                level=log.INFO);
 door_button = Button(4)
 last_refresh = 0
+log.info("Started door switch service.")
 while True:
-    print("Waiting for button to be pressed")
-    door_button.wait_for_press()
-    print("Button was pressed.")
-    door_button.wait_for_release()
-    if ((time() - last_refresh) > 300):
-        refresh_connection()
-        last_refresh = time()
-    print("Opening door...")
-    aio.send_data(door.key, 'OPENED')
-    sleep(1)
+    if ((time() - last_refresh) > 1200):
+      refresh_connection()
+      last_refresh = time()
+    log.info("Waiting for button to be pressed")
+    door_button.wait_for_press(timeout=300)
+    if (door_button.is_pressed):
+      door_button.wait_for_release()
+      log.info("Button was pressed.")
+      log.info("Opening door...")
+      aio.send_data(door.key, 'OPENED')
+      sleep(1)
+    else:
+      log.info("Noone pressed the button for 5mins.");
--- start_door_monitor.py
+++ start_door_monitor.py
@@ -2,52 +2,52 @@
 import RPi.GPIO as GPIO
 from time import sleep
 from Adafruit_IO import Client, RequestError, Feed
+import logging as log
+import os
 
 SOL_GATE = 12
 # GPIO.setwarnings(False)
 
 # Set to your Adafruit IO key.
-# Remember, your key is a secret,
-# so make sure not to publish it when you publish this code!
-ADAFRUIT_IO_KEY = '<key>'
+ADAFRUIT_IO_KEY = os.environ['ADAFRUIT_IO_KEY']
 
 # Set to your Adafruit IO username.
-# (go to https://accounts.adafruit.com to find your username)
-ADAFRUIT_IO_USERNAME = '<username>'
+ADAFRUIT_IO_USERNAME = os.environ['ADAFRUIT_IO_USERNAME']
 
 def refresh_connection():
-  print("Refreshing connection.")
+  log.info("Refreshing connection.")
   # Create an instance of the REST client.
   global aio
   aio = Client(ADAFRUIT_IO_USERNAME, ADAFRUIT_IO_KEY)
   try:
     global door
     door = aio.feeds('door')
-  except RequestError: # Doesn't exist, create a new feed
-    # feed = Feed(name="foo")
-    # foo = aio.create_feed(feed)
-    print('No feed called Door.')
+  except RequestError:
+    log.critical('No feed called Door.')
     quit()
 
 def open_door():
-  print("Opening door.\n")
+  log.info("Opening door.\n")
   try:
-    print("Unlatching on for 5 secs.")
+    log.info("Unlatching on for 5 secs.")
     GPIO.setmode(GPIO.BOARD)
     GPIO.setup(SOL_GATE, GPIO.OUT, initial=GPIO.LOW)
     GPIO.output(SOL_GATE, GPIO.HIGH)
     sleep(5)
-    print("Releasing...")
+    log.info("Releasing...")
     GPIO.setup(SOL_GATE, GPIO.IN)
-    print("Sleeping for 1 sec.")
+    log.info("Sleeping for 1 sec.")
     sleep(1)
   except KeyboardInterrupt:
-    print("\n.Received keyboard interrupt.\n")
+    log.critical("\n.Received keyboard interrupt.\n")
   finally:
-    print("Cleaning up.")
+    log.info("Cleaning up.")
     GPIO.setup(SOL_GATE, GPIO.IN)
     GPIO.cleanup()
 
+log.basicConfig(filename='/var/log/iot/start_door_monitor.log',
+                format='[%(levelname)s]%(asctime)s: %(message)s',
+                level=log.INFO);
 counter = 0
 while(True):
   if (counter % 300 == 0):
@@ -56,7 +56,7 @@ while(True):
 
   data = aio.receive(door.key)
   value = data.value
-  print('Retrieved value of : {0}'.format(data.value))
+  log.info('Retrieved value of : {0}'.format(data.value))
   
   if (value == 'OPENED'):
     open_door()
